8601876fb964e1ebd74b61d9d8dde1b4043290a3
Martin Roštejnský <martin.rostejnsky@email.cz>
2023-02-08 10:39:32 +0100

    Add project files.

---
+++ Birthday/Program.cs
@@ -0,0 +1,12 @@
+﻿DateTime birthday = DateTime.ParseExact(Console.ReadLine(), "dd.M.yyyy", null);
+TimeSpan age = new TimeSpan(DateTime.Now.Ticks - birthday.Ticks);
+
+Console.WriteLine(Math.Floor(age.TotalDays/365.25) + " let");
+
+Console.WriteLine(Math.Floor(age.TotalDays / 365.25) + " let " + (birthday.Month - DateTime.Now.Month) + " měsíců " + (birthday.Day - DateTime.Now.Day) + " dní " + (DateTime.Now.Hour - birthday.Hour) + " hodin "+ (DateTime.Now.Minute - birthday.Minute) + " minut.");
+
+Console.WriteLine(Math.Floor(age.TotalDays) + " dní");
+Console.WriteLine(Math.Floor(age.TotalSeconds) + " vteřin");
+Console.WriteLine(age.Ticks + " ticků");
+Console.WriteLine("Další narozeniny za " + ( 365 - Math.Ceiling(age.TotalDays % 365.25)) + " dní");
+Console.WriteLine("Další 1000deniny za " + (1000 - Math.Ceiling(age.TotalDays % 1000)) + " dní");
